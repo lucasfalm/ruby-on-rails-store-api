7b6930cffb944f1bb25925d3a379611576348cd4
flucas97 <feijolucas1997@gmail.com>
2020-05-03 18:58:23 -0300

    add migration kind to contact

--- notebook-api/config/environments/development.rb
+++ notebook-api/config/environments/development.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
   # fix auto reloading - checa atualização do arquivo
-  config.file_watcher = ActiveSupport::FileUploadChecker
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 
   # Do not eager load code on boot.
   config.eager_load = false
