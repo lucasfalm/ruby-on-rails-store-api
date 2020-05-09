23198c72dfb3a68ea6beaa59a70310233e3483fe
flucas97 <feijolucas1997@gmail.com>
2020-05-08 21:51:07 -0300

    configure birthdate to iso8601 format

--- notebook-api/app/serializers/contact_serializer.rb
+++ notebook-api/app/serializers/contact_serializer.rb
@@ -3,7 +3,7 @@ class ContactSerializer < ActiveModel::Serializer
 
   def attributes(*args)
     h = super(*args)
-    h[:birthdate] =  (I18n.l(object.birthdate) unless object.birthdate.blank?)
+    h[:birthdate] =  object.birthdate.to_time.iso8601 unless object.birthdate.blank?
     h[:kind] = object.kind.description
     h[:phones] = object.phones.map(&:number)
     h[:street] = object.address.street
