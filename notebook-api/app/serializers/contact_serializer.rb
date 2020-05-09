3affb715464d6f5bd8118f1a322aeb970407e9db
flucas97 <feijolucas1997@gmail.com>
2020-05-08 21:46:33 -0300

    configure adapter json api for serializer

--- notebook-api/app/serializers/contact_serializer.rb
+++ notebook-api/app/serializers/contact_serializer.rb
@@ -1,3 +1,13 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes(:id, :name, :email, :birthdate, :kind)
+
+  def attributes(*args)
+    h = super(*args)
+    h[:birthdate] =  (I18n.l(object.birthdate) unless object.birthdate.blank?)
+    h[:kind] = object.kind.description
+    h[:phones] = object.phones.map(&:number)
+    h[:street] = object.address.street
+    h[:city] = object.address.city
+    h
+  end
 end
