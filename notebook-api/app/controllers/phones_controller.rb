ff3cefebcba7c8961ad1208ef429259d57d52a17
flucas97 <feijolucas1997@gmail.com>
2020-05-11 20:53:39 -0300

    fix phones route

--- notebook-api/app/controllers/phones_controller.rb
+++ notebook-api/app/controllers/phones_controller.rb
@@ -7,10 +7,6 @@ class PhonesController < ApplicationController
     private
     # Use callbacks to share common setup or constraints between actions.
     def set_phones
-        if params[:contact_id]
-          @phones = Contact.find(params[:contact_id]).phones
-        else
-          @phones = Phones.where(id: params[:id])
-        end
+        @phones = Contact.find(params[:contact_id]).phones
     end
 end
