dd5bd98ca8a5cbca2a9b47f715128316b1919804
flucas97 <feijolucas1997@gmail.com>
2020-05-15 21:02:12 -0300

    create V1 and V2 API for contacts

--- notebook-api/config/routes.rb
+++ notebook-api/config/routes.rb
@@ -3,14 +3,29 @@ Rails.application.routes.draw do
 
   resources :auths, only: [:create]
 
-  resources :contacts do
-    # nested routes
-    resource :kind, only: [:show], path: 'relationships/kind'
+  scope module: 'v1' do
+    resources :contacts, :constraints => lambda { |request| request.params[:version] == "1" } do
+      # nested routes
+      resource :kind, only: [:show], path: 'relationships/kind'
 
-    resource :phones, only: [:show], path: '/phones'
+      resource :phones, only: [:show], path: '/phones'
 
-    resource :phone, only: [:create, :destroy, :update], path: '/phone'
+      resource :phone, only: [:create, :destroy, :update], path: '/phone'
 
-    resource :address, path: '/address'
+      resource :address, path: '/address'
+    end
+  end
+
+  scope module: 'v2' do
+    resources :contacts, :constraints => lambda { |request| request.params[:version] == "2" } do
+      # nested routes
+      resource :kind, only: [:show], path: 'relationships/kind'
+
+      resource :phones, only: [:show], path: '/phones'
+
+      resource :phone, only: [:create, :destroy, :update], path: '/phone'
+
+      resource :address, path: '/address'
+    end
   end
 end
