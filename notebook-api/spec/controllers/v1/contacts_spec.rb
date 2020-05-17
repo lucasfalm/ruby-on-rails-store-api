cf3afa43390819b3aca25f2fcbddc6aa3f76b2dc
flucas97 <feijolucas1997@gmail.com>
2020-05-16 22:00:30 -0300

    use symbols instead status code

--- notebook-api/spec/controllers/v1/contacts_spec.rb
+++ notebook-api/spec/controllers/v1/contacts_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe V1::ContactsController, :v1test, type: :controller do
     context "GET requests" do
-        it 'request index and returns 200 OK' do
+        it 'returns 200 OK' do
             request.accept = 'application/json'
             get :index
-            expect(response).to have_http_status(200)
+            expect(response).to have_http_status(:ok)
         end
     end
 end
