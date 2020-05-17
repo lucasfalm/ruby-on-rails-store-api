03e4b338b644c99eebd8c7ac17bcd34f5997babf
flucas97 <feijolucas1997@gmail.com>
2020-05-16 22:18:50 -0300

    rspec for show, with 200 OK

--- notebook-api/spec/controllers/v1/contacts_spec.rb
+++ notebook-api/spec/controllers/v1/contacts_spec.rb
@@ -7,6 +7,13 @@ describe V1::ContactsController, :v1test, type: :controller do
             get :index
             expect(response).to have_http_status(:ok)
         end
+        it 'show - returns 200 OK' do
+            request.accept = 'application/json'
+            contact = Contact.first
+            get :show, :params => { id: contact.id }
+            response_body = JSON.parse(response.body)
 
+            expect((response_body.fetch('data').fetch('id')).to_i).to eql(contact.id)
+        end
     end
 end
