a5d045e917dc98517afc6f05b9560e2f837a7145
flucas97 <feijolucas1997@gmail.com>
2020-05-16 22:09:16 -0300

    fix default url options in test

--- notebook-api/spec/controllers/v1/contacts_spec.rb
+++ notebook-api/spec/controllers/v1/contacts_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 describe V1::ContactsController, :v1test, type: :controller do
     context "GET requests" do
-        it 'returns 200 OK' do
+        it 'index - returns 200 OK' do
             request.accept = 'application/json'
             get :index
             expect(response).to have_http_status(:ok)
         end
+
     end
 end
