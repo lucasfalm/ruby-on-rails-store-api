adff0b76660e12df660f5aaa7887630606c5ac21
flucas97 <feijolucas1997@gmail.com>
2020-05-16 22:24:10 -0300

    reopen hash class to format json

--- notebook-api/spec/controllers/v1/contacts_spec.rb
+++ notebook-api/spec/controllers/v1/contacts_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
+class Hash
+    def json(parts)
+        ary = parts.split(">")
+        ary.reduce(self) do |memo, key|
+            memo.fetch(key.to_s.strip) if memo
+        end
+    end
+end
 describe V1::ContactsController, :v1test, type: :controller do
     context "GET requests" do
         it 'index - returns 200 OK' do
@@ -13,7 +21,7 @@ describe V1::ContactsController, :v1test, type: :controller do
             get :show, :params => { id: contact.id }
             response_body = JSON.parse(response.body)
 
-            expect((response_body.fetch('data').fetch('id')).to_i).to eql(contact.id)
+            expect(response_body.json('data > id').to_i).to eql(contact.id)
         end
     end
 end
